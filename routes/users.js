41312d29278962aff12160d6dc7820bdfd734ba3
ChiragDhawan <chiragdhawan16@gmail.com>
2023-07-06 04:26:52 +0530

    adding dependencies

--- routes/users.js
+++ routes/users.js
@@ -1,43 +1,8 @@
 import express from "express"
-import mongoose from "mongoose";
+import {getallusers} from "../controllers/userController.js"
 var router =express.Router()
 
 
-const UserSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    profilePic: { type: String, default: "" },
-    isAdmin: { type: Boolean, default: false },
-  },
-  { timestamps: true }
-);
-const User=mongoose.model("User", UserSchema);
-// module.exports = mongoose.model("User", UserSchema);
-
-const getallusers=async (req, res) => {
-    try {
-        const users=await User.find().sort({ _id: -1 })
-        if(users)
-        {
-            res.status(200).send({
-                success: true,
-                message: "Successfully got all users",
-                users,
-              });  
-        }
-       
-    } catch (error) {
-        
-        res.status(400).send({
-            success: false,
-            message: "Error in getting users all users",
-            error,
-          })
-    }
-    
-}
 
 router.get("/", getallusers);
 
