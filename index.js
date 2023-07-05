a75cf18425bd4ad43d3a8030098a0a931320a8b8
ChiragDhawan <chiragdhawan16@gmail.com>
2023-07-06 02:36:40 +0530

    adding dependencies

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose"
 
+dotenv.config();
 const app=express()
+app.use(express.json());
 
-app.listen(3000);
+const PORT=process.env.PORT||8080
+
+const connectDB=async()=>{
+    try {
+       
+       const conn=await mongoose.connect(process.env.MONGO_URL);
+       console.log("db connected")
+    } catch (error) {
+        console.log(error )
+    }
+}
+
+connectDB();
 
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
     console.log("Backend server is running!");
   });
